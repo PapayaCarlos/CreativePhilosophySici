a28334fdc3bb299f8b1c6ea4c3342ff2ca65e022
ziekhart <andy_xz@live.com>
2014-12-05 15:40:29 -0400

    Changed window layout, and formatting

--- SiciProject/src/Program/Transacciones.java
+++ SiciProject/src/Program/Transacciones.java
@@ -1,4 +1,6 @@
 package Program;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,7 +10,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+/**
+ * @authors github.com/ziekhart/, Andres A. Acevedo & Carlos Figueroa
+ */
 public class Transacciones {
 
 	public static void main(String[] args) {
@@ -21,38 +25,35 @@ public class Transacciones {
 		dblCantPagar = Double.parseDouble(cantPagar.getText());
 		dblCantPagada = Double.parseDouble(cantPagada.getText());
 		if(dblCantPagada < dblCantPagar)
-			strResult = "El dinero que le dio no es suficiente.";
-		else if( dblCantPagada == 0)
-			strResult = "Le falta poner la cantidad pagada";
-		else if(dblCantPagar ==0)
-			strResult = "Le falta poner la cantidad que va pagar.";
+			return "El dinero que le dio no es suficiente.";
 		else{	
-		result = dblCantPagada -dblCantPagar;
-		strResult = Double.toString(result);
+			result = dblCantPagada -dblCantPagar;
+			strResult = Double.toString(result);
+			return "Resultado: $"+ strResult;
 		}
-		return strResult;
+		
 	}
 
 	public static JFrame calcFrame(){
-		JFrame frame = new JFrame("Testing");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		JFrame frame = new JFrame("Calculador");
+		JButton button = new JButton("Resultado");
 		final JPanel p = new JPanel(new GridLayout (5,2,10,10));
 		final JTextField cantPagar = new JTextField(10);
 		final JTextField cantPagada = new JTextField(10);
 		final JLabel resultLabel = new JLabel();
+		frame.setResizable(false);
 		
-		//boolean check = true;
-		p.add(new JLabel("Cantidad de dinero que va pagar el cliente: "));
+		
+		p.add(new JLabel(" Cantidad de dinero que va pagar el cliente:  $"));
 		p.add(cantPagar);
-		p.add(new JLabel("Cantidad de dinero que le dio el cliente: "));
+		p.add(new JLabel(" Cantidad de dinero que le dio el cliente:        $"));
 		p.add(cantPagada);
-		
-		p.add(resultLabel);
+		p.add(button, FlowLayout.TRAILING);
+		p.add(new JLabel(""));
+		p.add(new JLabel(""));
+		p.add(resultLabel,BorderLayout.SOUTH);
 		resultLabel.setVisible(false);
-		JButton button = new JButton("Resultado");
-		p.add(button);
+		
 		button.addActionListener(new ActionListener()
 		{
 		public void actionPerformed(ActionEvent event){
@@ -60,9 +61,12 @@ public class Transacciones {
 			resultLabel.setVisible(true);
 		}
 		});
-
+		
+		frame.setResizable(false);
 		frame.add(p);
 		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		return frame;
 	}
 }
